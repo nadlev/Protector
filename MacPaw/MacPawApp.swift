93c244a5ed91c0ee66c8f95e29df64855eec37ef
nadlev <nadiya.levytska.jr@gmail.com>
2023-08-27 16:24:11 +0300

    Initial Commit

---
+++ MacPaw/MacPawApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPawApp.swift
+//  MacPaw
+//
+//  Created by 99999999 on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MacPawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
